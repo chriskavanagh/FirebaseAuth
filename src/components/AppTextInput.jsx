8b2e82fe686a0189856355b3657020fe10932e18
Chris Kavanagh <ckava3@gmail.com>
2020-08-11 23:19:22 -0400

    BTN

--- src/components/AppTextInput.jsx
+++ src/components/AppTextInput.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { View, TextInput, StyleSheet } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
-//<AntDesign name="edit" size={24} color="black" />
 
 export default function AppTextInput(props) {
   return (
