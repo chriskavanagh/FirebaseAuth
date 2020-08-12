0766e3a9d1fe11c0741e9ac9b2e2328c7264ac70
Chris Kavanagh <ckava3@gmail.com>
2020-08-12 16:22:55 -0400

    cleanup

--- src/components/AppTextInput.jsx
+++ src/components/AppTextInput.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { AntDesign } from "@expo/vector-icons";
 import { View, TextInput, StyleSheet } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
 
 export default function AppTextInput(props) {
   return (
