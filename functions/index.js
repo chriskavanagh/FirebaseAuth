f306943dcf965eaca112f0431a10ee9e09e0bbed
Chris Kavanagh <ckava3@gmail.com>
2020-09-21 19:51:06 -0400

    del ios android

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,22 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const functions = require("firebase-functions");
+const stripe = require("stripe")(
+  "sk_test_51HTJOZJpGJGqUhHBADROAxxgCMWUr1B7ylc3kLJP3KfxgCGOQTR2eaNcE9npIs4NQzO39MrcWSxHfb8IofRAKNjk00Xs54pJSz"
+);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.completePaymentWithStripe = functions.https.onRequest(
+  (request, response) => {
+    stripe.charge
+      .create({
+        amount: request.body.amount,
+        currency: request.body.currency,
+        source: "tok_mastercard",
+      })
+      .then((charge) => {
+        response.send(charge);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+);
