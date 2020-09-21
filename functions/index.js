dce61d92f9060b63b0634811f73e17ef73960dc5
Chris Kavanagh <ckava3@gmail.com>
2020-09-21 19:24:25 -0400

    firebase function

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,24 @@ const functions = require("firebase-functions");
 const stripe = require("stripe")(
   "sk_test_51HTJOZJpGJGqUhHBADROAxxgCMWUr1B7ylc3kLJP3KfxgCGOQTR2eaNcE9npIs4NQzO39MrcWSxHfb8IofRAKNjk00Xs54pJSz"
 );
+
+exports.completePaymentWithStripe = functions.https.onRequest(
+  (request, response) => {
+    stripe.charge
+      .create({
+        amount: request.body.amount,
+        currency: request.body.currency,
+        source: "tok_mastercard",
+      })
+      .then((charge) => {
+        response.send(charge);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+);
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
